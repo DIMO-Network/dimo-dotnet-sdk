1ad515ef70db32a8e73ade3f8965dd2dd17b66d8
Eduardo Rodriguez <eduardo.rodriguezz@outlook.com>
2025-02-19 21:39:32 +0100

    Add new privilege definitions

--- src/Dimo.Client/Models/PrivilegeSharing.cs
+++ src/Dimo.Client/Models/PrivilegeSharing.cs
@@ -6,5 +6,9 @@ namespace Dimo.Client.Models
         Commands = 2,
         CurrentLocation = 3,
         AllTimeLocation = 4,
+        ViewVinCredentials = 5,
+        LiveDataStreams = 6,
+        RawData = 7,
+        ApproximateLocation = 8,
     }
 }
